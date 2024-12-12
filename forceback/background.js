d54f066fe2feb4cd988214b82f94dc2867b4b693
Michael Basaman <mbasaman@gmail.com>
2024-12-12 02:03:07 -0500

    search

--- forceback/background.js
+++ forceback/background.js
@@ -1,5 +1,12 @@
 let url_map = new Map();
 
+chrome.tabs.onRemoved.addListener((tabId, removeInfo) => {
+    if(typeof tabId != "undefined"
+            && url_map.has(tabId)) {
+        url_map.delete(tabId);                
+    }
+});
+
 chrome.tabs.onUpdated.addListener((tabId, changeInfo, tab) => {
 	if(typeof tabId != "undefined"
 			&& typeof changeInfo != "undefined"
@@ -14,20 +21,36 @@ chrome.tabs.onUpdated.addListener((tabId, changeInfo, tab) => {
 			
 			if(last_url == tab.url) {
 				setTimeout(() => {
-					chrome.history.deleteRange({
-						endTime: millis + 10,
-						startTime: millis - 100
-					}, () => {
-						return true;
-					});	
-				}, 500);
+					chrome.history.search({
+						endTime: millis + 1000,
+						startTime: millis - 100,
+						text: ""
+					}).then((results) => {
+						if(typeof results != "undefined"
+								&& Array.isArray(results)) {
+							for(let i = 0; i < results.length; i++) {
+								let historyItem = results[i];
+								
+								if(typeof historyItem != "undefined"
+										&& typeof historyItem.lastVisitTime != "undefined"
+										&& typeof historyItem.url != "undefined"
+										&& historyItem.url == last_url) {
+									chrome.history.deleteRange({
+										endTime: historyItem.lastVisitTime + 1,
+										startTime: historyItem.lastVisitTime - 1
+									}).then(() => {
+										console.log("ForceBack chrome.history.deleteRange(" + millis + ", " + )
+									});		
+								}
+							}			
+						}
+					});
+				}, 1000);
 			} else {
 				url_map.set(tabId, tab.url);	
 			}
 		} else {
 			url_map.set(tabId, tab.url);
 		}
-	}	
-	
-	return true;
+	}
 });
